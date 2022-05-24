5fea023408472841333858cc0f04c7dfdd32f69a
andrew3213221 <cgjlby@gmail.com>
2022-05-24 10:30:39 +0200

    added PendingIntent.FLAG_IMMUTABLE

--- android/src/main/kotlin/io/flutter/plugins/geofencing/GeofencingPlugin.kt
+++ android/src/main/kotlin/io/flutter/plugins/geofencing/GeofencingPlugin.kt
@@ -161,7 +161,7 @@ class GeofencingPlugin : ActivityAware, FlutterPlugin, MethodCallHandler {
     private fun getGeofencePendingIndent(context: Context, callbackHandle: Long): PendingIntent {
       val intent = Intent(context, GeofencingBroadcastReceiver::class.java)
               .putExtra(CALLBACK_HANDLE_KEY, callbackHandle)
-      return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+      return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
     }
 
     @JvmStatic
